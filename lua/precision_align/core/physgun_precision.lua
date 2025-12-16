6798f68d8bdbd39c7ad574f4e7dfd91d5e28eeb3
march <106459595+marchc1@users.noreply.github.com>
2025-12-16 15:35:33 -0800

    Better physgun rotation clamp

---
+++ lua/precision_align/core/physgun_precision.lua
@@ -0,0 +1,22 @@
+if SERVER then
+    local function SnapTo(Value, SnapAngles)
+        return math.Round(Value / SnapAngles) * SnapAngles
+    end
+
+    hook.Add("OnPhysgunFreeze", "PA_FixPhysgunRotationInaccuracy", function(_, PhysObj, _, Player)
+        if not Player:KeyDown(IN_SPEED) then return end
+        local ShouldSnap = Player:GetInfo("precision_align_snap_physgun")
+        if ShouldSnap == 0 then return end
+
+        local SnapAngles = Player:GetInfo("gm_snapangles")
+        local Angles = PhysObj:GetAngles()
+        local P, Y, R = Angles:Unpack()
+        P = SnapTo(P, SnapAngles)
+        Y = SnapTo(Y, SnapAngles)
+        R = SnapTo(R, SnapAngles)
+        Angles:SetUnpacked(P, Y, R)
+        PhysObj:SetAngles(Angles)
+    end)
+else
+    CreateClientConVar("precision_align_snap_physgun", "1", true, true, "When enabled, this forces physgun snapping to always be 100% accurate to your desired gm_snapangles.", 0, 1)
+end
